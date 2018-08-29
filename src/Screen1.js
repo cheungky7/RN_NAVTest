ae3e819c8192c1221f522a51f316607056e551c9
cheungky <kycheung1985@gmail.com>
2018-08-29 23:56:49 +0800

    Add dropDown menu

--- src/Screen1.js
+++ src/Screen1.js
@@ -1,12 +1,38 @@
 import React from 'react'
 import { StyleSheet, Text, View, Image } from 'react-native'
+import { Dropdown } from 'react-native-material-dropdown';
+//import { width } from 'window-size';
+
+
+/*const typographyData = [
+  { value: 'Display2', label: 'Display 2' },
+  { value: 'Display1', label: 'Display 1' },
+  { value: 'Headline' },
+  { value: 'Title' },
+  { value: 'Subheading' },
+  { value: 'Body' },
+  { value: 'Caption' },
+];*/
+
+const locale=[
+ 
+  {value: 'en',label:'English'},
+  {value: 'tw',label:'中文'},
+
+];
 
 export default class Screen1 extends React.Component {
   
     render() {
       return (
         <View style={{flex:1,alignItems: 'center',justifyContent:'center'}}>
-          <Text>Screen 1</Text>
+          <Text>Setting</Text>
+          <Dropdown
+            containerStyle={{width: 100, height: 80,}}
+           
+            label='Language'
+            data={locale}
+           />
         </View>
       )
     }
