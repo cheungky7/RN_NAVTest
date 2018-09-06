c758fbf9cfd3e8fad4e5bcc740c807be1547393e
cheungky <kycheung1985@gmail.com>
2018-09-06 23:51:33 +0800

    Add some logic to track navigation event

--- src/Screen2.js
+++ src/Screen2.js
@@ -3,8 +3,23 @@ import { StyleSheet, Text, View, Image } from 'react-native'
 import I18n from './I18N/i18n'
 
 export default class Screen2 extends React.Component {
+
+  componentWillMount() {
+    this.willFocusSubscription = this.props.navigation.addListener('willFocus', payload => {
+      console.log(payload);
+    });
+    this.willBlurSubscription = this.props.navigation.addListener('willBlur', payload => {
+      console.log(payload);
+    });
+  }
+
+  componentWillUnount() {
+    this.willFocusSubscription.remove();
+    this.willBlurSubscription.remove();
+  }
   
     render() {
+      console.log(this.props);
       return (
         <View style={{flex:1,alignItems: 'center',justifyContent:'center'}}>
           <Text>{I18n.t('screen')} 2</Text>
